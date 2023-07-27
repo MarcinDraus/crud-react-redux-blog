c0c66b283e65a1625a2addba4a181cf7418a875f
MarcinDraus <marcin.drauss@gmail.com>
2023-07-27 06:06:50 +0200

    header, NavBar, footer

--- crud-react-redux-blog/src/App.js
+++ crud-react-redux-blog/src/App.js
@@ -7,12 +7,16 @@ import Post from './components/pages/Post/Post';
 import NotFound from './components/pages/NotFound/NotFound';
 import AddPost from './components/pages/AddPost/AddPost';
 import EditPost from './components/pages/EditPost/EditPost';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import { Container } from 'react-bootstrap'
 
 const App = () => {
     return (
 
-    
-        <Routes>
+    <Container>
+       <Header />
+       <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/post/:id" element={<Post />} />
             <Route path="/post/add" element={<AddPost />} />
@@ -20,7 +24,8 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
-    
+        <Footer />
+    </Container>
 
       );
     }
--- crud-react-redux-blog/src/Redux/initialState.js
+++ crud-react-redux-blog/src/Redux/initialState.js
@@ -2,15 +2,30 @@
 const initialState = {
     posts: [
         {
-          id: "1",
-          title: 'Things to do...',
-          description: 'Interesting things I want to check out'
-        },
+         id: '1',
+        title: 'Article title',
+        shortDescription: 'Short description of the article...',
+        content: 'Main content of the article',
+        publishedDate: '02-02-2022',
+        author: 'John Doe'
+      },
       {
-          id: "2",
-          title: 'Test list',
-          description: 'Lorem Ipsum'
-      }
+        id: '2',
+        title: 'Article title 2',
+        shortDescription: 'Short description of the article 2...',
+        content: 'Main content of the article 2',
+        publishedDate: '15-03-2022',
+        author: 'Jane Smith'
+      },
+      {
+        id: '3',
+        title: 'Article title 3',
+        shortDescription: 'Short description of the article 3...',
+        content: 'Main content of the article 3',
+        publishedDate: '10-05-2022',
+        author: 'Emily Johnson'
+      },
+      
     ],
 }
 export default initialState;
